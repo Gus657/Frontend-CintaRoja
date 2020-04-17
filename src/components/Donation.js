2fd6b47b4e67e37814167e99c09c5b2db122487b
siIvin <garavito.silvia@gmail.com>
2020-04-16 21:45:10 -0600

    Donation

--- src/components/Donation.js
+++ src/components/Donation.js
@@ -17,11 +17,11 @@ class Donation extends Component {
 
   render() { 
     return ( 
-      <Card className="mt-3" style={{ backgroundColor: '#ae51f5' }}>
+      <Card className="mt-3" style={{ backgroundColor: '#32CD32' }}>
         <CardBody>
           <CardTitle style={{ color: 'white' }}>{ this.state.suscriber }</CardTitle>
           <CardText style={{ color: 'white' }}>
-            {`Existencias: ${this.state.amount} - Precio: ${this.state.message}`}
+            {`Donación: ${this.state.amount} - Mensaje: ${this.state.message}`}
           </CardText>
         </CardBody>
       </Card>
