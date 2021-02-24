90ff5c4cfc502106fc7da1980dd3f4daedded669
Gus657 <ggusperezg@gmail.com>
2021-02-24 16:24:38 +0000

    New vwersion

---
+++ build/precache-manifest.3117f017ea2885c1626e805bcbdd403e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7216190be240b5b31ab0e3cae0d70003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de065cf93a3da610a5c3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "de065cf93a3da610a5c3",
+    "url": "/static/js/2.f4ac216e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f4ac216e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2dcd14987eecd84d8d",
+    "url": "/static/js/main.0b81286c.chunk.js"
+  },
+  {
+    "revision": "332b187e4e73f3c1119e",
+    "url": "/static/js/runtime-main.b22b5ad3.js"
+  }
+]);
