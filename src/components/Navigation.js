48f0e390c9e78bc19a5cbb9794ea7c3a351e32a5
Gustavo Perez <ggusperezg@gmail.com>
2021-02-27 20:39:00 -0600

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -25,7 +25,7 @@ const Navigation = (props) => {
           <Nav className="mr-auto" navbar>
             <NavItem>
               <NavLink>
-                <Link to="/suscribers">Test12</Link>
+                <Link to="/suscribers">Test12wer</Link>
               </NavLink>
             </NavItem>
             <NavItem>
